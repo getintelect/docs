6486edf34da5779df3c07cf5ab52e2717297cb1e
Emil <emilkeserovic@Outlook.com>
2022-05-15 16:16:53 +0200

    Replaced metadata

--- theme.config.js
+++ theme.config.js
@@ -12,7 +12,24 @@ export default {
   ),
   head: (
     <>
-      <meta name="og:title" content="Intelect" />
+      <!-- Primary Meta Tags -->
+      <meta name="title" content="Intelect | Reliable Tech">
+      <meta name="description" content="We are a Roblox technology group. We try to innovate new technologies for the RoAviation community.">
+
+      <!-- Open Graph / Facebook -->
+      <meta property="og:type" content="website">
+      <meta property="og:url" content="https://metatags.io/">
+      <meta property="og:title" content="Intelect | Reliable Tech">
+      <meta property="og:description" content="We are a Roblox technology group. We try to innovate new technologies for the RoAviation community.">
+      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+      <!-- Twitter -->
+      <meta property="twitter:card" content="summary_large_image">
+      <meta property="twitter:url" content="https://metatags.io/">
+      <meta property="twitter:title" content="Intelect | Reliable Tech">
+      <meta property="twitter:description" content="We are a Roblox technology group. We try to innovate new technologies for the RoAviation community.">
+      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
       <link
         rel="apple-touch-icon"
         sizes="180x180"
