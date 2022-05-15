dd437b00fb2b4517165c43fcd21ea2a66d93c20c
Emil <emilkeserovic@Outlook.com>
2022-05-15 16:52:38 +0200

    Fixed Image

--- theme.config.js
+++ theme.config.js
@@ -16,16 +16,16 @@ export default {
       <meta name="description" content="We are a Roblox technology group. We try to innovate new technologies for the RoAviation community." />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://metatags.io/" />
+      <meta property="og:url" content="https://www.intelect.cc/" />
       <meta property="og:title" content="Intelect | Reliable Tech" />
       <meta property="og:description" content="We are a Roblox technology group. We try to innovate new technologies for the RoAviation community." />
-      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      <meta property="og:image" content="/intelectbanner.png" />
 
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta property="twitter:url" content="https://www.intelect.cc/" />
       <meta property="twitter:title" content="Intelect | Reliable Tech" />
       <meta property="twitter:description" content="We are a Roblox technology group. We try to innovate new technologies for the RoAviation community." />
-      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      <meta property="twitter:image" content="/intelectbanner.png" />
 
       <link
         rel="apple-touch-icon"
